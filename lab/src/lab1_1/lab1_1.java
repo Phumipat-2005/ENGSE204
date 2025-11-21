a4b9131ff5466151f688b148f51b63a02d9a584d
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-21 13:41:11 +0700

    commit_lab

---
+++ lab/src/lab1_1/lab1_1.java
@@ -0,0 +1,13 @@
+package lab1_1;
+import java.util.Scanner;
+
+public class lab1_1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number1 = input.nextInt();
+		int number2 = input.nextInt();
+		System.out.println(number1 + number2);
+		input.close();
+	}
+}
