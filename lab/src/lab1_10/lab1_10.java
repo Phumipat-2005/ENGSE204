d9e83ce2b2d1e237d1ff36a67158c49075f92c07
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:08:53 +0700

    lab1_10

--- lab/src/lab1_10/lab1_10.java
+++ lab/src/lab1_10/lab1_10.java
@@ -1,19 +1,19 @@
 package lab1_10;
-import java.util.Scanner;	//	import การรับค่าจากผู้ใช้งานเข้ามา
+import java.util.Scanner;
 
 public class lab1_10 {
 	public static double calculateArea(double weight , double height) {
-		double result = weight * height;	//	นำค่า weight คููณกับ height
-		return result;	//	ส่งค่า result กลับ
-	}	//	End calculateArea
+		double result = weight * height;
+		return result;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Enter number");	//	เเสดงคำว่า	Enter number
-		Scanner input = new Scanner(System.in);	//	รับค่าจากผู้ใช้
-		double weight = input.nextDouble();	//	ค่าที่รับมาตัวที่ 1 ให้เป็น weight
-		double height = input.nextDouble();	//	ค่าที่รับมาตัวที่ 2 ให้เป็น height
-		double sum = calculateArea(weight , height);	//	ให้ sum เท่ากับ calculateArea
-		System.out.println(sum);	//	แสดงค่าของ sum ออกมา
-		input.close();	//	ปิดการรับค่าจากผู้ใช้
-	}	//	End main
+		System.out.println("Enter number");
+		Scanner input = new Scanner(System.in);
+		double weight = input.nextDouble();
+		double height = input.nextDouble();
+		double sum = calculateArea(weight , height);
+		System.out.println(sum);
+		input.close();
+	}
 }
