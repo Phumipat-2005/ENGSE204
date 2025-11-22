6e4cb0abf93bfc970f6ce628fa4f76b64f9c8315
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:42:03 +0700

    lab1_5

--- lab/src/lab1_5/lab1_5.java
+++ lab/src/lab1_5/lab1_5.java
@@ -1,22 +1,24 @@
 package lab1_5;
-import java.util.Scanner;	//	เพิ่มฟังค์ชั่นรับค่าจากผู้ใช้เข้ามา
+import java.util.Scanner;
 
 public class lab1_5 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);	// รับค่า input จากผู้ใช้งาน
-		int number001 = input.nextInt();	// ให้ number001 = ค่าที่กรอกเข้าไป	
-		input.close();	// ปิดการรับค่า
+		int number001;
+		System.out.println("Enter number :");
+		Scanner input = new Scanner(System.in);
+		number001 = input.nextInt();
+		input.close();
 		
-		if (number001 == 1 ) {	//	เมื่อกรอกค่าเข้ามา = 1
-			System.out.println("Americano");	//	ให้เเสดงคำว่า Americano
-		} else if (number001 == 2) {	//	เมื่อกรอกค่าเข้ามา = 2
-			System.out.println("Latte");	//	ให้เเสดงคำว่า Latte
-		} else if (number001 == 3 ) {	//	เมื่อกรอกค่าเข้ามา = 3
-			System.out.println("Espresso");	//	ให้เเสดงคำว่า Espresso
-		} else if (number001 == 4 ) {	//	เมื่อกรอกค่าเข้ามา = 4
-			System.out.println("Mocha");	//	ให้เเสดงคำว่า Mocha
-		} else	// ถ้าค่าที่กรอกมาไม่ใช่ 1 - 4 
-			System.out.println("Invalid Menu");	//	ให้เเสดงคำว่า Invalid Menu
+		if (number001 == 1 ) {
+			System.out.println("Americano");
+		} else if (number001 == 2) {
+			System.out.println("Latte");
+		} else if (number001 == 3 ) {
+			System.out.println("Espresso");
+		} else if (number001 == 4 ) {
+			System.out.println("Mocha");
+		} else
+			System.out.println("Invalid Menu");
 	}
 }
