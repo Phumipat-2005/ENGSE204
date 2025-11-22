a87d7d4e5b5dff796bd3b928e85a3b822a6b6ce1
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 20:13:13 +0700

    lab1_9

--- lab/src/lab1_9/lab1_9.java
+++ lab/src/lab1_9/lab1_9.java
@@ -5,21 +5,21 @@ public class lab1_9 {
 	public static void main(String[] args) {
 		System.out.print("Enter number : ");
 		Scanner input = new Scanner(System.in);
-		int array = input.nextInt();
-		int [] allarray = new int [array];
+		int array001 = input.nextInt();
+		int [] allarray001 = new int [array001];
 		
-		for (int i = 0; i < allarray.length; i++) {
+		for (int i = 0; i < allarray001.length; i++) {
 			int arraynum = input.nextInt();
-			allarray[i] = arraynum;
+			allarray001[i] = arraynum;
 		}
 
 		int sum = 0;
-        for (int i = 0; i < allarray.length; i++) {
-        	sum += allarray[i];
+        for (int i = 0; i < allarray001.length; i++) {
+        	sum += allarray001[i];
         }
         
-        double average = (double) sum / allarray.length;
-        System.out.println(average);
+        double average = (double) sum / allarray001.length;
+        System.out.println("Output : " + average);
         input.close();
 	}
 }
