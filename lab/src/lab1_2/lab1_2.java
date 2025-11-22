38c9199b3ebf2ef99afb597a702b28012bd5264e
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:09:47 +0700

    lab1_2

--- lab/src/lab1_2/lab1_2.java
+++ lab/src/lab1_2/lab1_2.java
@@ -1,13 +1,13 @@
 package lab1_2;
-import java.util.Scanner;	//	import รับค่าจากผู้ใช้งาน
+import java.util.Scanner;
 
 public class lab1_2 {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		float number001 = input.nextFloat();	//	ให้ค่าที่กรอกเข้ามาตัวที่ 1 เป็น number001
-		float number002 = input.nextFloat();	//	ให้ค่าที่กรอกเข้ามาตัวที่ 2 เป็น number002
-		System.out.println(number001 * number002);	//	แสดงผลลัพธ์ของ number001 * number002
-		input.close();	//	ปิดการรับค่าจากผู้ใช้งาน
-	}	// End main
-}	// End Class
+		float number001 = input.nextFloat();
+		float number002 = input.nextFloat();
+		System.out.println(number001 * number002);
+		input.close();
+	}
+}
