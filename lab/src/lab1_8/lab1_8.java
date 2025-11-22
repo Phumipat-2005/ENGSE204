75112f24d5c0a44bf18702d1c3702746c141aa11
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:56:52 +0700

    lab1_8

--- lab/src/lab1_8/lab1_8.java
+++ lab/src/lab1_8/lab1_8.java
@@ -1,5 +1,5 @@
 package lab1_8;
-import java.util.Scanner;	//	เพิ่มฟังค์ชั่นรับค่าจากผู้ใช้เข้ามา
+import java.util.Scanner;
 
 public class lab1_8 {
 
@@ -12,16 +12,16 @@ public class lab1_8 {
 		for (int i = 0; i < allarray.length; i++) {
 			int arraynum = input.nextInt();
 			allarray[i] = arraynum;
-		}	//	End For Loop
+		}
 		
 		int max = allarray[0];
         for (int i = 1; i < allarray.length; i++) {
             if (allarray[i] > max) {
                 max = allarray[i];
-            }	// end if
-        }	//	End For Loop
+            }
+        }
         
-        System.out.println("Maxinum score = " + max);	// ให้เเสดง Maxinum score = ผลลัพธ์
-        input.close();	// ปิดการรับค่า
+        System.out.println("Maxinum score = " + max);
+        input.close();
 	}
 }
