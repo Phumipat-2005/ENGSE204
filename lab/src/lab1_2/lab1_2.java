8018b958e3cfcff6d74964914ca576b836ec8a12
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-22 19:25:13 +0700

    lab1_2

--- lab/src/lab1_2/lab1_2.java
+++ lab/src/lab1_2/lab1_2.java
@@ -2,11 +2,13 @@ package lab1_2;
 import java.util.Scanner;
 
 public class lab1_2 {
-	
 	public static void main(String[] args) {
+		float number001;
+		float number002;
+		System.out.println("Enter number :");
 		Scanner input = new Scanner(System.in);
-		float number001 = input.nextFloat();
-		float number002 = input.nextFloat();
+		number001 = input.nextFloat();
+		number002 = input.nextFloat();
 		System.out.println(number001 * number002);
 		input.close();
 	}
