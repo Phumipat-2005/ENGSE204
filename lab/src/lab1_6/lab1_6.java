35d471358a019f488e5a4be5b5e6e89e4b320445
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-23 14:43:20 +0700

    lab1_6

--- lab/src/lab1_6/lab1_6.java
+++ lab/src/lab1_6/lab1_6.java
@@ -4,23 +4,17 @@ import java.util.Scanner;
 public class lab1_6 {
 
 	public static void main(String[] args) {
-		int number001;
-		System.out.println("Enter number :");
+		int number;
+		System.out.print("Enter number : ");
 		Scanner input = new Scanner(System.in);
-		number001 = input.nextInt();
-		input.close();
+		number = input.nextInt();
 		
-		System.out.println(number001 + " x 1 = " + number001 * 1);
-		System.out.println(number001 + " x 2 = " + number001 * 2);
-		System.out.println(number001 + " x 3 = " + number001 * 3);
-		System.out.println(number001 + " x 4 = " + number001 * 4);
-		System.out.println(number001 + " x 5 = " + number001 * 5);
-		System.out.println(number001 + " x 6 = " + number001 * 6);
-		System.out.println(number001 + " x 7 = " + number001 * 7);
-		System.out.println(number001 + " x 8 = " + number001 * 8);
-		System.out.println(number001 + " x 9 = " + number001 * 9);
-		System.out.println(number001 + " x 10 = " + number001 * 10);
-		System.out.println(number001 + " x 11 = " + number001 * 11);
-		System.out.println(number001 + " x 12 = " + number001 * 12);
+		System.out.println("Multiplication table of " + number);
+		
+		for (int i = 1; i <= 12; i++) {
+			System.out.println(number + " x " + i + " = " + (number * i));
+		}
+		
+		input.close();
 	}
 }
