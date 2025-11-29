bbca5cf6708ccc0e9cd2a185cb7bda8bd918dbaf
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-29 20:37:26 +0700

    lab2_2

--- lab/src/lab2_2/lab2_2.java
+++ lab/src/lab2_2/lab2_2.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 public class lab2_2 {
 	
-	private String studentID;
+	private String studentid;
 	private String name;
 	
-	public void displayInfo() {
-		System.out.println("รหัสนักศึกษา: " + studentID);
-		System.out.println("ชื่อ-นามสกุล: " + name);
+	public void displayinfo() {
+		System.out.println("------- Output -------");
+		System.out.println("Studentid: " + studentid);
+		System.out.println("Name: " + name);
 	}
 
 public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ป้อนรหัสนักศึกษา: ");
-        String inputStudentId = scanner.nextLine();
-        System.out.print("ป้อนชื่อ-นามสกุล: ");
-        String inputName = scanner.nextLine();
-        lab2_2 myStudent = new lab2_2();
-        myStudent.studentID = inputStudentId;
-        myStudent.name = inputName;
+        System.out.print("Studentid: ");
+        String inputstudentid = scanner.nextLine();
+        System.out.print("Name: ");
+        String inputname = scanner.nextLine();
+        lab2_2 student = new lab2_2();
+        student.studentid = inputstudentid;
+        student.name = inputname;
 
-        myStudent.displayInfo();
+        student.displayinfo();
         scanner.close();
     }
 }
