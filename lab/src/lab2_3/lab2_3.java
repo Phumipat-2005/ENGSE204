8725c6d054331a0ab5c191cc7368170c71feae67
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-11-30 10:41:38 +0700

    lab2_3

---
+++ lab/src/lab2_3/lab2_3.java
@@ -0,0 +1,31 @@
+package lab2_3;
+import java.util.Scanner;
+
+public class lab2_3 {
+	
+	private String studentid;
+	private String name;
+	private static int studentcount = 0;
+	
+	public lab2_3(String studentid, String name) {
+		this.studentid = studentid;
+		this.name = name;
+		studentcount++;
+	}
+	
+	public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("enter number student: ");
+			int n = scanner.nextInt();
+			
+			for (int i = 0; i < n; i++) {
+				String inputid = scanner.next();
+				String inputname = scanner.next();
+				lab2_3 newstudent = new lab2_3(inputid, inputname);
+			}
+			
+			System.out.println("-------Output-------");
+			System.out.println(lab2_3.studentcount);
+			scanner.close();
+	}
+}
