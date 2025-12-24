fca67ba040e24e20a3984915518df09298b89c58
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-12-24 21:26:56 +0700

    lab5_5

---
+++ lab/src/lab5_5/lab5_5.java
@@ -0,0 +1,67 @@
+package lab5_5;
+import java.util.Scanner;
+
+class Shape {
+	
+	protected String name;
+	
+	public Shape(String name) {
+		this.name = name;
+	}
+	
+	public double calculateArea() {
+		return 0.0;
+	}
+}
+
+class Circle extends Shape {
+	
+	protected double radius;
+	
+	public Circle(String name, double radius) {
+		super(name);
+		this.radius = radius;
+	}
+	
+	@Override
+	public double calculateArea() {
+		return 3.14159 * radius * radius;
+	}
+}
+
+class Rectangle extends Shape {
+	
+	protected double width;
+	protected double height;
+
+	public Rectangle(String name, double width, double height) {
+		super(name);
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	public double calculateArea() {
+		return width * height;
+	}
+}
+
+public class lab5_5 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		if (scanner.hasNextDouble()) {
+			double radius = scanner.nextDouble();
+			double width = scanner.nextDouble();
+			double height = scanner.nextDouble();
+			Shape c = new Circle("Circle", radius);
+			Shape r = new Rectangle("Rectangle", width, height);
+			Shape[] shapes = {c, r};
+			System.out.println("--------Output---------");
+			for (Shape s : shapes) {
+				System.out.println(s.calculateArea());
+			}
+		}
+		scanner.close();
+	}
+}
